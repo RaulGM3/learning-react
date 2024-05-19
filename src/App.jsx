afa03c8120f979b93eb6503fa01d674e24426287
RaulGM3 <apaladario@gmail.com>
2024-05-19 12:17:09 -0400

    spread the object so you don't have to pass every prop.

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function genRandomInt (max) {
 function CoreConcepts (props) {
   return (
     <li>
-      <img src={props.img} alt="" />
+      <img src={props.image} alt="" />
       <h3>{props.title}</h3>
       <p>{props.description}</p>
     </li>
@@ -39,10 +39,11 @@ function App() {
         <section id="core-concepts">
         <h2>Core Concepts</h2>
         <ul>
-          <CoreConcepts title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} img={CORE_CONCEPTS[0].image}></CoreConcepts>
-          <CoreConcepts title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} img={CORE_CONCEPTS[1].image}></CoreConcepts>
-          <CoreConcepts title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} img={CORE_CONCEPTS[2].image}></CoreConcepts>
-          <CoreConcepts title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} img={CORE_CONCEPTS[3].image}></CoreConcepts>
+          <CoreConcepts title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}></CoreConcepts>
+          {/* You can ... the whole object as well */}
+          <CoreConcepts {...CORE_CONCEPTS[1]}></CoreConcepts> 
+          <CoreConcepts title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} image={CORE_CONCEPTS[2].image}></CoreConcepts>
+          <CoreConcepts title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image}></CoreConcepts>
         </ul>
         </section>
       </main>
