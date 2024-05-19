a232b6e1d4247e8b9d922b945125f164a73fe389
RaulGM3 <apaladario@gmail.com>
2024-05-19 12:15:05 -0400

    Learning about props and components

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import reactImg from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from './data.js';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt (max) {
+  return Math. floor (Math. random () * (max + 1));
+}
+
+function CoreConcepts (props) {
+  return (
+    <li>
+      <img src={props.img} alt="" />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  )
+}
+
+function Header () {
+  return (
+    <header>
+      <img src={ reactImg } alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        { reactDescriptions[genRandomInt (2)] } React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <main>
+        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+        <h2>Core Concepts</h2>
+        <ul>
+          <CoreConcepts title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} img={CORE_CONCEPTS[0].image}></CoreConcepts>
+          <CoreConcepts title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} img={CORE_CONCEPTS[1].image}></CoreConcepts>
+          <CoreConcepts title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} img={CORE_CONCEPTS[2].image}></CoreConcepts>
+          <CoreConcepts title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} img={CORE_CONCEPTS[3].image}></CoreConcepts>
+        </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
